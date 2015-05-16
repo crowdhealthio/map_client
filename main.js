07a4e65f10f5d4f1af355e5d32efed05feb17868
Niket Shah <masterniket@gmail.com>
2015-05-16 12:34:32 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Provide your access token
+L.mapbox.accessToken = 'pk.eyJ1IjoibmlrZXQiLCJhIjoiY2NJM1RkZyJ9.yrIhCrWderoS8-l1dluHEw';
+// Create a map in the div #map
+var map = L.mapbox.map('map', 'niket.895829ed');
